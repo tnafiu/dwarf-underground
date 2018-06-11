712877c25d28f790908792a61d20e725e3d8075c
Toluwa Nafiu <nafiutc@rose-hulman.edu>
2018-06-11 16:10:47 -0400

    Added an Ad and Author component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Header from './Header'
+import Author from './Author'
+import Ad from './Ad.js'
 
 class App extends Component {
   render() {
     return ( 
       <div className="App">
 
-        <Header />
+        <Header/>
 
         <main className="expanded row">
           <div className="large-8 medium-12 columns article">
             <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
-            <div className="avatar">
-              <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
-              <div className="author-info">
-                <p className="author-name">By Domri, son of Flug</p>
-                <p className="date">on 28 April, 3018 of the Third Age</p>
-              </div>
-            </div>
+            <Author/>
             <section className="article-body">
               <p><span className="article-start gold">E</span>psum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
 
@@ -46,20 +42,7 @@ class App extends Component {
               </a>
             </div>
           </div>
-          <aside className="large-4 medium-12 columns">
-            <div className="ad">
-              <h3>The Last Hammer You'll Ever Need</h3>
-              <div className="ad-image">
-                <img src="https://s-media-cache-ak0.pinimg.com/originals/b4/d5/ab/b4d5ab4a82b8f7efc1adaf548df1d089.jpg" alt="hammer" />
-                <a href="#">visit sponsor</a>
-              </div>  
-              <h4>Mining diamonds or crushing the skulls of your enemies, the SmashMaster 4000 does it all</h4>
-              <p className="ad-text">Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
-
-              <p className="ad-text">Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
-            </div>
-          </aside>
-
+          <Ad />
           <div className="small-12 columns other-articles">
             <h2>From around the Realm</h2>
             <div className="small-6 medium-3 columns other-article">
